81c4dffd656e879c903bf880c2f0606191613f35
unknown <benjaminfranklin085@gmail.com>
2020-07-06 08:23:16 +0100

    make it better

--- resources/views/tinker.blade.php
+++ resources/views/tinker.blade.php
@@ -36,6 +36,7 @@
     <div class="content" id="app">
         <botman-tinker api-endpoint="/botman"></botman-tinker>
         <form action="/users/1" method="POST">
+            {{ csrf }}
             <input type="text" name="Name">
             <button type="submit">Submit</button>
         </form>
